1a319f157fbd71b272a2f8f39a51faa160abe517
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-04-02 13:13:56 +0300

    KSH-76: Search method implementation.

---
+++ koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/resource/impl/SearchResourceImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+
+package fi.vm.sade.koulutusinformaatio.resource.impl;
+
+import fi.vm.sade.koulutusinformaatio.resource.SearchResource;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+@Component
+@Path("/search")
+public class SearchResourceImpl implements SearchResource {
+
+    @Override
+    @GET
+    @Path("/{text}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object search(@PathParam("text") String text) {
+        return "NOT IMPLEMENTED - search key: " + text;
+    }
+}
