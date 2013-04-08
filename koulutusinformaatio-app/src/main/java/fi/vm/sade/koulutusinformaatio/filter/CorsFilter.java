e6f4bf016acb2554b8da50e2ee2b06e16b43c223
majapuro <mikko.majapuro@gofore.com>
2013-04-08 16:27:12 +0300

    Cors filter

---
+++ koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/filter/CorsFilter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+
+package fi.vm.sade.koulutusinformaatio.filter;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+/**
+ * @author Mikko Majapuro
+ */
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public ContainerResponse filter(ContainerRequest containerRequest, ContainerResponse containerResponse) {
+
+        if ( containerRequest.getRequestHeaders().containsKey("access-control-request-method") ) {
+            for ( String value : containerRequest.getRequestHeaders().get("access-control-request-method") ) {
+                containerResponse.getHttpHeaders().add("Access-Control-Allow-Methods", value );
+            }
+        }
+        if ( containerRequest.getRequestHeaders().containsKey("access-control-request-headers") ) {
+            for ( String value : containerRequest.getRequestHeaders().get("access-control-request-headers") ) {
+                containerResponse.getHttpHeaders().add("Access-Control-Allow-Headers", value );
+            }
+        }
+        containerResponse.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+        return containerResponse;
+    }
+}
