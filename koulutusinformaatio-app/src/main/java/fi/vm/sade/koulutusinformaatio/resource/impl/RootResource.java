5ef11ee1e1a079d918c074792ad61fe1bf124e46
majapuro <mikko.majapuro@gofore.com>
2013-04-09 15:00:40 +0300

    Root resource

---
+++ koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/resource/impl/RootResource.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+
+package fi.vm.sade.koulutusinformaatio.resource.impl;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author Mikko Majapuro
+ */
+@Component
+@Path("/")
+public class RootResource {
+
+    @GET
+    public Response getRoot() throws URISyntaxException {
+        return Response.seeOther(new URI("/app")).build();
+    }
+}
