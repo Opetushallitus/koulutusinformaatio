c59c04a78eddedb4b7ff82eacf89d540265679cd
Kalle Lundahn <kalle.lundahn@proactum.fi>
2013-04-29 15:04:04 +0300

    Unit tests updated

--- koulutusinformaatio-app/src/main/webapp/test/unit/kiAppSpec.js
+++ koulutusinformaatio-app/src/main/webapp/test/unit/kiAppSpec.js
@@ -11,58 +11,105 @@ describe('SearchController', function() {
 
     beforeEach(module('kiApp'));
 
-    describe('when initial query string is given', function() {
-        beforeEach(inject(function($httpBackend, $rootScope, $controller, LearningOpportunity, ParentLearningOpportunity, SearchService, $location){
-            $httpBackend.when('GET', '../lo/search/' + searchterms.kasityo).respond([]);
-            $httpBackend.when('GET', '../lo/search/' + searchterms.musiikki).respond([]);
-            $httpBackend.when('GET', '../lo/search' + searchterms.empty).respond([]);
+    describe('when initial query string is given in path', function() {
+        var $httpBackend_;
+        var $controller_ ;
+        var LearningOpportunity_;
+        var ParentLearningOpportunity_;
+        var SearchService_;
+        var $location_;
+
+        beforeEach(inject(function($httpBackend, $rootScope, $controller, LearningOpportunity, 
+                ParentLearningOpportunity, SearchService, $location) {
+            $httpBackend_ = $httpBackend;
+            $controller_ = $controller;
+            LearningOpportunity_ = LearningOpportunity;
+            ParentLearningOpportunity_ = ParentLearningOpportunity;
+            SearchService_ = SearchService;
+            $location_ = $location;
             scope = $rootScope.$new();
-            ctrl = $controller(SearchCtrl, {$scope: scope, 
-                $routeParams: {queryString: searchterms.kasityo}, 
-                LearningOpportunity: LearningOpportunity,
-                ParentLearningOpportunity: ParentLearningOpportunity,
-                SearchService: SearchService,
-                $location: $location
-            });
         }));
 
+        afterEach(function() {
+            $httpBackend_.verifyNoOutstandingExpectation();
+            $httpBackend_.verifyNoOutstandingRequest();
+        });
+
+        it('should have scope variables set when an empty query string is given', function() {
+            spyOn(LearningOpportunity_, 'query').andCallThrough();
+            
+            ctrl = $controller_(SearchCtrl, {
+                $scope: scope, 
+                $routeParams: {}, 
+                LearningOpportunity: LearningOpportunity_,
+                ParentLearningOpportunity: ParentLearningOpportunity_,
+                SearchService: SearchService_,
+                $location: $location_
+            });
+
+            expect(LearningOpportunity_.query).not.toHaveBeenCalled();
+            expect(scope.queryString).toBeUndefined();
+            expect(scope.loResult).toBeUndefined();
+        });
+
         it('should have scope variables set when a proper query string is given', function() {
+            spyOn(LearningOpportunity_, 'query').andCallThrough();
+
+            $httpBackend_.when('GET', '../lo/search/' + searchterms.kasityo).respond(200, '[]');
+            ctrl = $controller_(SearchCtrl, {
+                $scope: scope, 
+                $routeParams: {queryString: searchterms.kasityo}, 
+                LearningOpportunity: LearningOpportunity_,
+                ParentLearningOpportunity: ParentLearningOpportunity_,
+                SearchService: SearchService_,
+                $location: $location_
+            });
+            $httpBackend_.flush();
+
+            expect(LearningOpportunity_.query).toHaveBeenCalled();
+            expect(LearningOpportunity_.query.calls.length).toEqual(1);
             expect(scope.queryString).toEqual(searchterms.kasityo);
             expect(scope.loResult.length).toEqual(0);
-            //expect(scope.showFilters).toBeTruthy();
         });
 
         it('should return correct result for manual search with valid query string', function() {
+            ctrl = $controller_(SearchCtrl, {
+                $scope: scope, 
+                $routeParams: {}, 
+                LearningOpportunity: LearningOpportunity_,
+                ParentLearningOpportunity: ParentLearningOpportunity_,
+                SearchService: SearchService_,
+                $location: $location_
+            });
+
             scope.queryString = searchterms.musiikki;
             scope.search();
-            expect(scope.loResult.length).toEqual(0);
-            //expect(scope.showFilters).toBeTruthy();
+
+            expect(SearchService_.getTerm()).toMatch(searchterms.musiikki);
+            expect($location_.path()).toMatch('/haku/' + searchterms.musiikki);
         });
 
         it('should return correct result for manual search with an empty query string', function() {
+            ctrl = $controller_(SearchCtrl, {
+                $scope: scope, 
+                $routeParams: {}, 
+                LearningOpportunity: LearningOpportunity_,
+                ParentLearningOpportunity: ParentLearningOpportunity_,
+                SearchService: SearchService_,
+                $location: $location_
+            });
+
+            var locationPrev = $location_.path();
+            var searchTermPrev = SearchService_.getTerm();
+
             scope.queryString = searchterms.empty;
             scope.search();
-            expect(scope.loResult.length).toEqual(0);
-            //expect(scope.showFilters).toBeFalsy();
+            
+            // location and serach term should not be updated when searching with an empty string
+            expect($location_.path()).toMatch(locationPrev);
+            expect(SearchService_.getTerm).toMatch(searchTermPrev);
         });
     });
-
-describe('when initial query string is not given', function() {
-    beforeEach(inject(function($httpBackend, $rootScope, $controller, LearningOpportunity){
-        $httpBackend.when('GET', '../lo/search/' + searchterms.kasityo).respond([]);
-        $httpBackend.when('GET', '../lo/search/' + searchterms.musiikki).respond([]);
-        $httpBackend.when('GET', '../lo/search' + searchterms.empty).respond([]);
-        scope = $rootScope.$new();
-        ctrl = $controller(SearchCtrl, {$scope: scope, $routeParams: {}, LearningOpportunity: LearningOpportunity});
-    }));
-
-    it('should have scope varables set when a proper query string is given', function() {
-        expect(scope.queryString).toBeUndefined();
-        expect(scope.loResult).toBeUndefined();
-        expect(scope.showFilters).toBeUndefined();
-    });
-});
-
 });
 
 describe('InfoController', function() {
@@ -70,22 +117,97 @@ describe('InfoController', function() {
 
     beforeEach(module('kiApp'));
 
-    describe('when parent LO is selected', function() {
-        beforeEach(inject(function($httpBackend, $rootScope, $controller, ParentLearningOpportunity, SearchService, $location){
-            $httpBackend.when('GET', '../lo/123456').respond({});
+    describe('when LearningOpportunity is selected', function() {
+        var $httpBackend_;
+        var $controller_;
+        var ParentLearningOpportunity_;
+        var SearchService_;
+        var LODataService_;
+        var TitleService_;
+        var $location_;
+
+        beforeEach(inject(function($httpBackend, $rootScope, $controller, ParentLearningOpportunity, 
+                SearchService, LODataService, TitleService) {
+            $httpBackend_ = $httpBackend;
+            $controller_ = $controller;
+            ParentLearningOpportunity_ = ParentLearningOpportunity;
+            SearchService_ = SearchService;
+            LODataService_ = LODataService;
+            TitleService_ = TitleService;
             scope = $rootScope.$new();
-            ctrl = $controller(IndexCtrl, {
-                $scope: scope, 
-                $routeParams: {parentId: '123456'}, 
-                ParentLearningOpportunity: ParentLearningOpportunity,
-                SearchService: SearchService,
-                $location: $location
-            });
+
         }));
 
+        afterEach(function() {
+            $httpBackend_.verifyNoOutstandingExpectation();
+            $httpBackend_.verifyNoOutstandingRequest();
+        });
+
         it('should fetch parent data', function() {
-            //expect(scope.parentLO.length).toEqual(0);
-            //expect(scope.showFilters).toBeTruthy();
+            var parentId = '123456';
+
+            $httpBackend_.when('GET', '../lo/123456').respond(200, {"id": parentId, "name": "parent name"});
+            ctrl = $controller_(InfoCtrl, {
+                $scope: scope, 
+                $routeParams: {parentId: '123456'}, 
+                ParentLearningOpportunity: ParentLearningOpportunity_,
+                SearchService: SearchService_,
+                LODataService: LODataService_,
+                TitleService: TitleService_
+            });
+            $httpBackend_.flush();
+
+            expect(scope.parentLO).not.toBeUndefined();
+            expect(scope.childLO).toBeUndefined();
+            expect(scope.parentLO.id).toMatch(parentId);
+        });
+
+        it('should fetch parent and child data', function() {
+            var parentId = '123456';
+            var childId = parentId + '_2';
+
+            $httpBackend_.when('GET', '../lo/123456').respond(200, {"id": parentId, "name": "parent name", "children": [{"id": "123456_1"}, {"id": childId}]});
+            ctrl = $controller_(InfoCtrl, {
+                $scope: scope, 
+                $routeParams: {parentId: '123456', childId: '123456_2'}, 
+                ParentLearningOpportunity: ParentLearningOpportunity_,
+                SearchService: SearchService_,
+                LODataService: LODataService_,
+                TitleService: TitleService_
+            });
+            $httpBackend_.flush();
+
+            expect(scope.parentLO).not.toBeUndefined();
+            expect(scope.childLO).not.toBeUndefined();
+            expect(scope.parentLO.id).toMatch(parentId);
+            expect(scope.childLO.id).toMatch(childId);
+        });
+    });
+});
+
+describe('IndexController', function() {
+    var ctrl, scope;
+
+    beforeEach(module('kiApp'));
+
+    describe('when index page is viewed', function() {
+        var $controller_;
+        var TitleService_;
+
+        beforeEach(inject(function($rootScope, $controller, TitleService) {
+            $controller_ = $controller;
+            TitleService_ = TitleService;
+            scope = $rootScope.$new();
+
+        }));
+
+        it('should have the correct title', function() {
+            ctrl = $controller_(IndexCtrl, {
+                $scope: scope, 
+                TitleService: TitleService_
+            });
+
+            expect(TitleService_.getTitle()).toContain('Etusivu');
         });
     });
 });
