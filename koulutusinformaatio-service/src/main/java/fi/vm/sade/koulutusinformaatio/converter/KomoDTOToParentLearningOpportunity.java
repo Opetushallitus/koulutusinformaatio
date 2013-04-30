dde5a81b96eaae68efc458d23e8a754df48d2998
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-04-30 12:20:10 +0300

    HAK-310: new ConversionServiceFactoryBean that gives converters access to ConversionService + few othen converter changes.

---
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/converter/KomoDTOToParentLearningOpportunity.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+
+package fi.vm.sade.koulutusinformaatio.converter;
+
+import fi.vm.sade.koulutusinformaatio.domain.I18nText;
+import fi.vm.sade.koulutusinformaatio.domain.ParentLearningOpportunity;
+import fi.vm.sade.tarjonta.service.resources.dto.KomoDTO;
+import fi.vm.sade.tarjonta.service.resources.dto.MonikielinenTekstisDTO;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+public class KomoDTOToParentLearningOpportunity implements Converter<KomoDTO, ParentLearningOpportunity> {
+
+    private ConversionService conversionService;
+
+    public KomoDTOToParentLearningOpportunity(ConversionService conversionService) {
+        this.conversionService = conversionService;
+    }
+
+    @Override
+    public ParentLearningOpportunity convert(KomoDTO source) {
+        ParentLearningOpportunity parent = new ParentLearningOpportunity();
+        parent.setId(source.getOid());
+        parent.setName("dummy name");
+        //parent.setName(conversionService.convert(source.getNimi(), I18nText.class));
+        return parent;
+    }
+
+}
---
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/converter/MonikielinenTekstisDTOToI18nText.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+
+package fi.vm.sade.koulutusinformaatio.converter;
+
+import fi.vm.sade.koulutusinformaatio.domain.I18nText;
+import fi.vm.sade.tarjonta.service.resources.dto.MonikielinenTekstisDTO;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+public class MonikielinenTekstisDTOToI18nText implements Converter<MonikielinenTekstisDTO, I18nText> {
+    @Override
+    public I18nText convert(MonikielinenTekstisDTO source) {
+        return null;
+    }
+}
