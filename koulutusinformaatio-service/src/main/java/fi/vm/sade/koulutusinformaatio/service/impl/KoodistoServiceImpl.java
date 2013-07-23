11f3a805a871d3882c4a243056a871919b2fc982
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-07-23 15:07:00 +0300

    Migrated to koodisto REST client

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/KoodistoServiceImpl.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/KoodistoServiceImpl.java
@@ -21,13 +21,11 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Strings;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
-import com.googlecode.ehcache.annotations.Cacheable;
 import fi.vm.sade.koodisto.service.GenericFault;
-import fi.vm.sade.koodisto.service.KoodiService;
 import fi.vm.sade.koodisto.service.types.SearchKoodisCriteriaType;
 import fi.vm.sade.koodisto.service.types.common.KoodiType;
 import fi.vm.sade.koodisto.service.types.common.KoodiUriAndVersioType;
-import fi.vm.sade.koodisto.service.types.common.SuhteenTyyppiType;
+import fi.vm.sade.koodisto.util.CachingKoodistoClient;
 import fi.vm.sade.koodisto.util.KoodiServiceSearchCriteriaBuilder;
 import fi.vm.sade.koulutusinformaatio.domain.Code;
 import fi.vm.sade.koulutusinformaatio.domain.CodeUriAndVersion;
@@ -40,7 +38,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -50,14 +47,14 @@ import java.util.regex.Pattern;
 @Service
 public class KoodistoServiceImpl implements KoodistoService {
 
-    private final KoodiService koodiService;
+    private final CachingKoodistoClient koodiService;
     private final ConversionService conversionService;
     private final Pattern pattern;
     private static final String KOODI_URI_WITH_VERSION_PATTERN = "^[^#]+#\\d+$";
     public static final Logger LOGGER = LoggerFactory.getLogger(KoodistoServiceImpl.class);
 
     @Autowired
-    public KoodistoServiceImpl(final KoodiService koodiService, final ConversionService conversionService) {
+    public KoodistoServiceImpl(final CachingKoodistoClient koodiService, final ConversionService conversionService) {
         this.koodiService = koodiService;
         this.conversionService = conversionService;
         this.pattern = Pattern.compile(KOODI_URI_WITH_VERSION_PATTERN);
@@ -186,7 +183,6 @@ public class KoodistoServiceImpl implements KoodistoService {
         }
     }
 
-    @Cacheable(cacheName = "subKoodiCache")
     private List<KoodiType> searchSubKoodiTypes(final String koodiUriAndVersion, final String koodistoURI) throws KoodistoException {
         return Lists.newArrayList(Collections2.filter(searchSubKoodiTypes(koodiUriAndVersion), new Predicate<KoodiType>() {
             @Override
@@ -199,10 +195,9 @@ public class KoodistoServiceImpl implements KoodistoService {
     private List<KoodiType> searchSubKoodiTypes(String koodiUriAndVersion) throws KoodistoException {
         CodeUriAndVersion codeUriAndVersion = resolveKoodiUriAndVersion(koodiUriAndVersion);
         KoodiUriAndVersioType koodiUriAndVersionType = conversionService.convert(codeUriAndVersion, KoodiUriAndVersioType.class);
-        return koodiService.listKoodiByRelation(koodiUriAndVersionType, false, SuhteenTyyppiType.SISALTYY);
+        return koodiService.getAlakoodis(codeUriAndVersion.getUri());
     }
 
-    @Cacheable(cacheName = "superKoodiCache")
     private List<KoodiType> searchSuperKoodiTypes(final String koodiUriAndVersion, final String koodistoURI) throws KoodistoException {
         return Lists.newArrayList(Collections2.filter(searchSuperKoodiTypes(koodiUriAndVersion), new Predicate<KoodiType>() {
             @Override
@@ -215,10 +210,9 @@ public class KoodistoServiceImpl implements KoodistoService {
     private List<KoodiType> searchSuperKoodiTypes(String koodiUriAndVersion) throws KoodistoException {
         CodeUriAndVersion codeUriAndVersion = resolveKoodiUriAndVersion(koodiUriAndVersion);
         KoodiUriAndVersioType koodiUriAndVersionType = conversionService.convert(codeUriAndVersion, KoodiUriAndVersioType.class);
-        return koodiService.listKoodiByRelation(koodiUriAndVersionType, true, SuhteenTyyppiType.SISALTYY);
+        return koodiService.getYlakoodis(codeUriAndVersion.getUri());
     }
 
-    @Cacheable(cacheName = "koodiCache")
     private List<KoodiType> searchKoodiTypes(String koodiUri) throws KoodistoException {
         CodeUriAndVersion codeUriAndVersion = resolveKoodiUriAndVersion(koodiUri);
         return getKoodiTypes(codeUriAndVersion);
