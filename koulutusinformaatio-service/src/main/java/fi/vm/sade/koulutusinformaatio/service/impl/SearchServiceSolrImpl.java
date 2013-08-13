305fa4b3e792023e10ba5b7207dc574a6f421617
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-08-13 16:00:46 +0300

    Logger implementation debug print added

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/SearchServiceSolrImpl.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/SearchServiceSolrImpl.java
@@ -100,6 +100,9 @@ public class SearchServiceSolrImpl implements SearchService {
     @Override
     public LOSearchResultList searchLearningOpportunities(String term, String prerequisite,
                                                           List<String> cities, int start, int rows) throws SearchException {
+
+        System.out.println("LOGGER IMPL DEBUG: " + LOG.getClass().toString());
+
         LOSearchResultList searchResultList = new LOSearchResultList();
         String trimmed = term.trim();
         if (!trimmed.isEmpty()) {
