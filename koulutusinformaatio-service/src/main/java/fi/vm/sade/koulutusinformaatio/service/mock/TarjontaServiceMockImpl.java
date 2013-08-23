9acb24872d2e5d2e489281feaed7549c2f14925c
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-08-23 14:28:19 +0300

    Tarjonta mock impl improved

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/mock/TarjontaServiceMockImpl.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/mock/TarjontaServiceMockImpl.java
@@ -21,18 +21,54 @@ import java.util.*;
 public class TarjontaServiceMockImpl implements TarjontaService {
 
     private static final String PARENT_KOMO_OID = "1.2.3.4.5";
-    private static final String PARENT_LOS_ID = "11.22.33.44.55";
-    private static final String PARENT_LOI_ID = "111.222.333.444.555";
-    private static final String CHILD_LO_ID = "1111.2222.3333.4444.5555";
-    private static final String CHILD_LOI_ID = "1111.2222.3333.4444.66666";
+
+    private static final String PARENT_LOS_ID_1 = "11.22.33.44.55";
+    private static final String PARENT_LOS_ID_2 = "211.22.33.44.55";
+
+    private static final String PARENT_LOI_ID_1 = "111.222.333.444.555";
+    private static final String PARENT_LOI_ID_2 = "2111.222.333.444.555";
+
+    private static final String CHILD_LOS_ID_1 = "1111.2222.3333.4444.5555";
+    private static final String CHILD_LOS_ID_2 = "21111.2222.3333.4444.5555";
+
+    private static final String CHILD_LOI_ID_1 = "1111.2222.3333.4444.66666";
+    private static final String CHILD_LOI_ID_2 = "21111.2222.3333.4444.66666";
+
     private static final String APPLICATION_SYSTEM_ID = "11111.22222.33333.44444.55555";
-    private static final String APPLICATION_OPTION_ID = "111111.222222.333333.444444.555555";
+
+    private static final String APPLICATION_OPTION_ID_1 = "111111.222222.333333.444444.555555";
+    private static final String APPLICATION_OPTION_ID_2 = "2111111.222222.333333.444444.555555";
+
     private static final String PROVIDER_ID = "1111111.2222222.3333333.4444444.5555555";
 
 
     @Override
     public List<ParentLOS> findParentLearningOpportunity(String oid) throws TarjontaParseException, KoodistoException {
-        return Lists.newArrayList(createParentLOS());
+
+        Provider provider = createProvider();
+        ApplicationSystem as = createApplicationSystem(APPLICATION_SYSTEM_ID);
+
+        I18nText parent1Name = createI18nText("Audiovisuaalisen viestinnän perustutkinto");
+        ParentLOSRef parent1Ref = createParentLORef(PARENT_LOS_ID_1, parent1Name);
+        ChildLOIRef childLOIRef1 = createChildLORef(CHILD_LOI_ID_1, CHILD_LOS_ID_1, APPLICATION_SYSTEM_ID);
+        ApplicationOption ao1 = createApplicationOption(APPLICATION_OPTION_ID_1,
+                createI18nText("Audiovisuaalisen viestinnän perustutkinto, pk"), as, Lists.newArrayList(childLOIRef1) ,parent1Ref);
+        ChildLOI childLOI1 = createChildLOI(CHILD_LOI_ID_1, Lists.newArrayList(ao1));
+        ChildLOS childLOS1 = createChildLOS(CHILD_LOS_ID_1,Lists.newArrayList(childLOI1));
+        ParentLOI parentLOI1 = createParentLOI(PARENT_LOI_ID_1, Lists.newArrayList(childLOIRef1), Sets.newHashSet(ao1));
+        ParentLOS parent1 = createParentLOS(PARENT_LOI_ID_1, parent1Name, Lists.newArrayList(parentLOI1), provider, Lists.newArrayList(childLOS1));
+
+        I18nText parent2Name = createI18nText("Audiovisuaalisen viestinnän perustutkinto tutkinto 2");
+        ParentLOSRef parent2Ref = createParentLORef(PARENT_LOS_ID_2, parent2Name);
+        ChildLOIRef childLOIRef2 = createChildLORef(CHILD_LOI_ID_2, CHILD_LOS_ID_2, APPLICATION_SYSTEM_ID);
+        ApplicationOption ao2 = createApplicationOption(
+                APPLICATION_OPTION_ID_2, createI18nText("Audiovisuaalisen viestinnän perustutkinto2, pk"), as, Lists.newArrayList(childLOIRef2), parent2Ref);
+        ChildLOI childLOI2 = createChildLOI(CHILD_LOI_ID_2, Lists.newArrayList(ao2));
+        ChildLOS childLOS2 = createChildLOS(CHILD_LOS_ID_2,Lists.newArrayList(childLOI2));
+        ParentLOI parentLOI2 = createParentLOI(PARENT_LOI_ID_2, Lists.newArrayList(childLOIRef2), Sets.newHashSet(ao2));
+        ParentLOS parent2 = createParentLOS(PARENT_LOI_ID_2, parent2Name, Lists.newArrayList(parentLOI2), provider, Lists.newArrayList(childLOS2));
+
+        return Lists.newArrayList(parent1, parent2);
     }
 
     @Override
@@ -47,12 +83,12 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         return listParentLearnignOpportunityOids();
     }
 
-    private ParentLOS createParentLOS() {
+    private ParentLOS createParentLOS(String id, I18nText name, List<ParentLOI> lois, Provider provider, List<ChildLOS> children) {
         ParentLOS parentLOS = new ParentLOS();
-        parentLOS.setId(PARENT_LOS_ID);
-        parentLOS.setName(createI18nText("Audiovisuaalisen viestinnän perustutkinto"));
-        parentLOS.setLois(Lists.newArrayList(createParentLOI()));
-        parentLOS.setProvider(createProvider());
+        parentLOS.setId(id);
+        parentLOS.setName(name);
+        parentLOS.setLois(lois);
+        parentLOS.setProvider(provider);
         parentLOS.setStructureDiagram(createI18nText(
                 "<p>Tutkinnon kaikille pakollinen tutkinnonosa on audiovisuaalinen tuotanto (30 ov). Lisäksi suoritetaan valinnaisia osia, jotka tukevat ammatillista suuntautumista.</p>  <p>Ammatillisten tutkinnon osien laajuus on yhteensä 90 opintoviikkoa. (Näyttötutkinnossa tutkinnon osilla ei ole laajuuksia.) Ammatillisessa peruskoulutuksessa (opetussuunnitelmaperusteisessa) opiskellaan lisäksi 20 ov ammattitaitoa täydentäviä ja 10 ov vapaasti valittavia tutkinnon osia.</p>"));
         parentLOS.setAccessToFurtherStudies(createI18nText("<p>Ammatillisista perustutkinnoista sekä ammatti- ja erikoisammattitutkinnoista saa yleisen jatko-opintokelpoisuuden ammattikorkeakouluihin ja yliopistoihin. Luonteva jatko-opintoväylä esim. ammatillisen perustutkinnon suorittaneilla on artenomin, muotoilijan tai konservaattorin ammattikorkeakoulututkinnot. Aalto-yliopiston Taideteollisessa korkeakoulussa tai Lapin yliopistossa voi suorittaa esimerkiksi taiteen kandidaatin tai taiteen maisterin tutkinnot. Ammatillisen opettajan pedagogiset opinnot antavat jatkokoulutusmahdollisuuden ammatillisen opettajan työtehtäviin. Tekniikan kandidaatin, diplomi-insinöörin tai arkkitehdin tutkintoja voi suorittaa teknillisissä korkeakouluissa ja yliopistoissa.</p>"));
@@ -63,23 +99,23 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         parentLOS.setEducationDegree("32");
         parentLOS.setCreditValue("120");
         parentLOS.setCreditUnit(createI18nText("opintoviikko"));
-        parentLOS.setChildren(Lists.newArrayList(createChildLOS()));
+        parentLOS.setChildren(children);
         return parentLOS;
     }
 
-    private ParentLOI createParentLOI() {
+    private ParentLOI createParentLOI(String id, List<ChildLOIRef> refs, Set<ApplicationOption> aos) {
         ParentLOI parentLOI = new ParentLOI();
-        parentLOI.setId(PARENT_LOI_ID);
+        parentLOI.setId(id);
         parentLOI.setSelectingEducation(createI18nText("<p>Opintojen valinta teksti</p>"));
         parentLOI.setPrerequisite(new Code("PK", createI18nText("Peruskoulu"), createI18nText("Peruskoulu")));
-        parentLOI.setChildRefs(Lists.newArrayList(createChildLORef()));
-        parentLOI.setApplicationOptions(Sets.newHashSet(createApplicationOption()));
+        parentLOI.setChildRefs(refs);
+        parentLOI.setApplicationOptions(aos);
         return parentLOI;
     }
 
-    private ChildLOS createChildLOS() {
+    private ChildLOS createChildLOS(String id, List<ChildLOI> lois) {
         ChildLOS childLOS = new ChildLOS();
-        childLOS.setId(CHILD_LO_ID);
+        childLOS.setId(id);
         childLOS.setName(createI18nText("Audiovisuaalisen viestinnän koulutusohjelma, media-assistentti",
                 "Utbildningsprogrammet för audiovisuell kommunikation, medieassistent", "Audio visual communication study program, media assistant",
                 "audiovisuaalinen, assistentti", "audiovisuaalinen, assistentti", "audiovisuaalinen, assistentti"));
@@ -89,14 +125,14 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         childLOS.setDegreeGoal(createI18nText("<p>Audiovisuaalisen viestinnän perustutkinnon tavoitteena on antaa opiskelijalle tarvittava perusosaaminen alan assistenttitason työtehtäviin, jotka liittyvät viestintätuotteiden suunnitteluun, toteuttamiseen, valmistamiseen, tuottamiseen ja markkinointiin.</p>  <p>Audiovisuaalisilla viestintätuotteilla tarkoitetaan mm. valokuvia, elokuvia, video-, radio- ja tv- ohjelmia, multimediatuotteita, vuorovaikutteisia pelejä ja - ohjelmia, digitaalisia elokuvatallenteita, webbisivuja ja wap- palveluja. Perustutkinnon tavoitteissa on painotettu teknistuotannollisen tietotaidon lisäksi esteettisfilosofista perusnäkemystä sekä kulttuurihistorian perusteiden tuntemusta.</p>  <p>Audiovisuaalisen viestinnän alalla on useita laajoja ammatteja ja ammattiryhmiä, siksi alalla tarvitaan sekä moniosaajia että erikoistuneita tietyn tekniikan tai taidon erityisosaajia. Tämän takia alan perustutkinnon tavoitteissa korostuu alan kaikille opiskelijoille yhteinen perusosaaminen sekä laaja valinnaisten opintojen osuus, joka mahdollistaa hyvin erilaisten suuntautumisvaihtoehtojen toteuttamisen.</p>",
                 "<p>Målet med grundexamen i audiovisuell kommunikation är att ge dig de nödvändiga baskunskaperna i de arbetsuppgifter på assistentnivå inom branschen som hänför sig till planering, genomförande, framställning, produktion och marknadsföring av medieprodukter. Här avses med audiovisuella medieprodukter bl.a. fotografier, filmer, video-, radio- och TV-program, multimedieprodukter, interaktiva spel och program, digitala filminspelningar, webbsidor och wap-tjänster. I målen för grundexamen betonas utöver kunnande i teknisk produktion en estetisk-filosofisk grundsyn samt kunskap i kulturhistoriens grunder.</p>  <p>Inom branschen för audiovisuell kommunikation finns flera yrken och yrkesgrupper och därför behövs både personer med kunskaper inom flera olika områden och personer som är specialiserade på en viss teknik eller färdighet. I grundexamen betonas en gemensam baskompetens för alla studerande samt en omfattande andel valfria studier som möjliggör olika inriktningsalternativ.</p>",
                 "<p>Degree goal in english</p>"));
-        childLOS.setLois(Lists.newArrayList(createChildLOI()));
+        childLOS.setLois(lois);
         return childLOS;
     }
 
-    private ChildLOI createChildLOI() {
+    private ChildLOI createChildLOI(String id, List<ApplicationOption> aos) {
         ChildLOI childLOI = new ChildLOI();
-        childLOI.setId(CHILD_LOI_ID);
-        childLOI.setApplicationOptions(Lists.newArrayList(createApplicationOption()));
+        childLOI.setId(id);
+        childLOI.setApplicationOptions(aos);
         Calendar cal = new GregorianCalendar();
         cal.set(Calendar.YEAR, 2014);
         cal.set(Calendar.MONTH, 8);
@@ -117,11 +153,11 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         return childLOI;
     }
 
-    private ChildLOIRef createChildLORef() {
+    private ChildLOIRef createChildLORef(String loiId, String losId, String asId) {
         ChildLOIRef childLOIRef = new ChildLOIRef();
-        childLOIRef.setId(CHILD_LO_ID);
-        childLOIRef.setLosId(CHILD_LO_ID);
-        childLOIRef.setAsIds(Lists.newArrayList(APPLICATION_SYSTEM_ID));
+        childLOIRef.setId(loiId);
+        childLOIRef.setLosId(losId);
+        childLOIRef.setAsIds(Lists.newArrayList(asId));
         childLOIRef.setName(createI18nText("Audiovisuaalisen viestinnän koulutusohjelma, media-assistentti",
                 "Utbildningsprogrammet för audiovisuell kommunikation, medieassistent", "Audio visual communication study program, media assistant",
                 "audiovisuaalinen, assistentti", "audiovisuaalinen, assistentti", "audiovisuaalinen, assistentti"));
@@ -131,9 +167,9 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         return childLOIRef;
     }
 
-    private ApplicationSystem createApplicationSystem() {
+    private ApplicationSystem createApplicationSystem(String id) {
         ApplicationSystem as = new ApplicationSystem();
-        as.setId(APPLICATION_SYSTEM_ID);
+        as.setId(id);
         as.setName(createI18nText("Yhteishaku 2013"));
         Calendar start = new GregorianCalendar();
         start.set(Calendar.YEAR, 2013);
@@ -148,14 +184,14 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         return as;
     }
 
-    private ApplicationOption createApplicationOption() {
+    private ApplicationOption createApplicationOption(String id, I18nText name, ApplicationSystem as, List<ChildLOIRef> childLOIRefs, ParentLOSRef pRef) {
         ApplicationOption ao = new ApplicationOption();
-        ao.setId(APPLICATION_OPTION_ID);
-        ao.setName(createI18nText("Audiovisuaalisen viestinnän perustutkinto, pk"));
+        ao.setId(id);
+        ao.setName(name);
         ao.setAoIdentifier("452");
-        ao.setApplicationSystem(createApplicationSystem());
+        ao.setApplicationSystem(as);
         ao.setEducationDegree("32");
-        ao.setChildLOIRefs(Lists.newArrayList(createChildLORef()));
+        ao.setChildLOIRefs(childLOIRefs);
         ao.setProvider(createProvider());
         ao.setStartingQuota(10);
         ao.setLowestAcceptedScore(5);
@@ -165,7 +201,7 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         ao.setLastYearApplicantCount(10);
         ao.setSora(true);
         ao.setTeachingLanguages(Lists.newArrayList("FI", "EN"));
-        ao.setParent(createParentLORef());
+        ao.setParent(pRef);
         ao.setSelectionCriteria(createI18nText("<p>Valintaperustekuvaus</p>"));
         ao.setPrerequisite(new Code("PK", createI18nText("Peruskoulu"), createI18nText("Peruskoulu")));
         ao.setRequiredBaseEducations(Lists.newArrayList("6", "2", "7", "0", "1", "3"));
@@ -173,10 +209,10 @@ public class TarjontaServiceMockImpl implements TarjontaService {
         return ao;
     }
 
-    private ParentLOSRef createParentLORef() {
+    private ParentLOSRef createParentLORef(String id, I18nText name) {
         ParentLOSRef parentLORef = new ParentLOSRef();
-        parentLORef.setId(PARENT_LOS_ID);
-        parentLORef.setName(createI18nText("Audiovisuaalisen viestinnän perustutkinto"));
+        parentLORef.setId(id);
+        parentLORef.setName(name);
         return parentLORef;
     }
 
