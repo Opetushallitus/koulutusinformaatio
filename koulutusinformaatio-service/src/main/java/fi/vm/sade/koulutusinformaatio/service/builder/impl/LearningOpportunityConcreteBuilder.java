d63a919bee33a177e7cd6ab947cc246d66b7d4d6
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2013-09-03 13:16:29 +0300

    Debug System.out.println removed

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/builder/impl/LearningOpportunityConcreteBuilder.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/builder/impl/LearningOpportunityConcreteBuilder.java
@@ -271,11 +271,6 @@ public class LearningOpportunityConcreteBuilder implements LearningOpportunityBu
     }
 
     private ChildLOS createChildLOS(KomoDTO childKomo, String childLOSId, List<KomotoDTO> childKomotos) throws KoodistoException {
-
-        if (childLOSId.equals("1.2.246.562.5.2013061010184614873316_1.2.246.562.10.74355268397")) {
-            System.out.println("");
-        }
-
         ChildLOS childLOS = new ChildLOS();
         childLOS.setId(childLOSId);
         childLOS.setName(koodistoService.searchFirst(childKomo.getKoulutusOhjelmaKoodiUri()));
