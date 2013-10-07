d80bd78c62a48e6efd5db72bb33a949eff1bec26
Kalle Lundahn <kalle.lundahn@proactum.fi>
2013-10-07 15:11:53 +0300

    Default piwik tracking added to KI angular app

--- koulutusinformaatio-app/src/main/webapp/app/lib/angulartics/angulartics-piwik.js
+++ koulutusinformaatio-app/src/main/webapp/app/lib/angulartics/angulartics-piwik.js
@@ -5,14 +5,16 @@
 	.config(['$analyticsProvider', function ($analyticsProvider) {
 
 		$analyticsProvider.registerPageTrack(function (path) {
-			if(_paq) {
-				_paq.push(['setDocumentTitle', document.domain + "/" + document.title]);
-				_paq.push(['trackPageView', path]);
+			if (_paq) {
+				var documentTitle = document.domain + '/' + document.title
+				_paq.push(['setDocumentTitle', documentTitle]);
+				_paq.push(['setCustomUrl', path]);
+				_paq.push(['trackPageView', documentTitle]);
 			}
 		});
 
 		$analyticsProvider.registerSiteSearchTrack(function (keyword, category, resultCount) {
-			if(_paq) _paq.push(['trackSiteSearch', keyword, category, resultCount]);
+			if (_paq) _paq.push(['trackSiteSearch', keyword, category, resultCount]);
 		});
 
 		$analyticsProvider.registerEventTrack(function (action, properties) {
