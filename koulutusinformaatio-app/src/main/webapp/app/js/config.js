e1212dbee5cf71b8427f9ed42ed169d017360227
Kalle Lundahn <kalle.lundahn@proactum.fi>
2013-12-16 15:08:51 +0200

    config updated

--- koulutusinformaatio-app/src/main/webapp/app/js/config.js
+++ koulutusinformaatio-app/src/main/webapp/app/js/config.js
@@ -27,33 +27,33 @@ window.Config.app.prod = {
 
 window.Config.app.koulutus = {
 	fi: {
-		frontpageUrl: '/',
-		rekisteriselosteUrl: '/',
+		frontpageUrl: '/wp/fi/',
+		rekisteriselosteUrl: '/wp/fi/'
 	},
 	sv: {
-		frontpageUrl: '/',
-    	rekisteriselosteUrl: '/',
-	},
+		frontpageUrl: '/wp/sv/',
+    	rekisteriselosteUrl: '/wp/sv/'
+	}
 };
 
 window.Config.app.qa = {
 	fi: {
 		frontpageUrl: '/wp/fi/',
-		rekisteriselosteUrl: '/wp/fi/rekisteriseloste/',
+		rekisteriselosteUrl: '/wp/fi/rekisteriseloste/'
 	},
 	sv: {
 		frontpageUrl: '/wp/sv/',
-    	rekisteriselosteUrl: '/wp/sv/registerbeskrivning/',
-	},
+    	rekisteriselosteUrl: '/wp/sv/registerbeskrivning/'
+	}
 };
 
 window.Config.app.dev = {
 	fi: {
-		frontpageUrl: '/',
-		rekisteriselosteUrl: '/',
+		frontpageUrl: '/wp/fi/',
+		rekisteriselosteUrl: '/wp/fi/'
 	},
 	sv: {
-		frontpageUrl: '/',
-    	rekisteriselosteUrl: '/',
-	},
+		frontpageUrl: '/wp/sv/',
+    	rekisteriselosteUrl: '/wp/sv/'
+	}
 };
