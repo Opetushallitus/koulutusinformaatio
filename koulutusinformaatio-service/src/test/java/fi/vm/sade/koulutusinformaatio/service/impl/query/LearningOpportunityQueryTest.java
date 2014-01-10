87ac5b766f1f98ed2e8bc50d62434ea914afd405
mholi <markus.holi@proactum.fi>
2014-01-10 09:01:27 +0200

    OVT-6484: testaja

--- koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/service/impl/query/LearningOpportunityQueryTest.java
+++ koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/service/impl/query/LearningOpportunityQueryTest.java
@@ -19,6 +19,8 @@ package fi.vm.sade.koulutusinformaatio.service.impl.query;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+
+import org.apache.solr.common.params.DisMaxParams;
 import org.junit.Test;
 
 import java.util.List;
@@ -31,21 +33,23 @@ import static org.junit.Assert.*;
 public class LearningOpportunityQueryTest {
 
     private static final String TERM = "term";
-    private static final String STAR = "*";
     private static final String PREREQUISITE = "PK";
     private static final List<String> CITIES = Lists.newArrayList("city1", "city2");
-    private static final List<String> FACET_FILTERS = Lists.newArrayList("teachingLang:suomi", "teachingLang:ruotsi");
+    private static final List<String> FACET_FILTERS = Lists.newArrayList("someOther:vaat", "someOther:vaat2");
+    private static final List<String> FF_TEACH_LANG_FI = Lists.newArrayList( "teachingLangCode_ffm:FI", "someOther_ffm:whatever");
+    private static final List<String> FF_TEACH_LANG_SV = Lists.newArrayList( "teachingLangCode_ffm:SV", "someOther_ffm:whatever");
+    private static final List<String> FF_TEACH_LANG_OTHER = Lists.newArrayList( "teachingLangCode_ffm:OT", "someOther_ffm:whatever");
     private static final boolean ONGOING = false;
     private static final boolean UPCOMING = false;
-    private static final String LANG = "fi";
+    private static final String LANG_FI = "fi";
     private static final int START = 0;
     private static final int ROWS = 10;
     private static final String SORT = "0";
     private static final String ORDER = "asc";
 
     @Test
-    public void testQuery() {
-        LearningOpportunityQuery q = new LearningOpportunityQuery(TERM, PREREQUISITE, CITIES, FACET_FILTERS, LANG, ONGOING, UPCOMING, START, ROWS, SORT, ORDER);
+    public void testQueryTeachLangNone() {
+        LearningOpportunityQuery q = new LearningOpportunityQuery(TERM, PREREQUISITE, CITIES, FACET_FILTERS, LANG_FI, ONGOING, UPCOMING, START, ROWS, SORT, ORDER);
         assertNotNull(q);
         assertEquals(6, q.getFilterQueries().length);
         String prerequisiteFQ = new StringBuilder("prerequisites:").append(PREREQUISITE).toString();
@@ -55,6 +59,58 @@ public class LearningOpportunityQueryTest {
         assertEquals(lopHomeplaceFQ, q.getFilterQueries()[1]);
         assertEquals(TERM, q.getQuery().toString());
         assertEquals("edismax", q.getParams("defType")[0]);
+        assertEquals(Joiner.on(" ").join(LearningOpportunityQuery.FIELDS), q.getParams(DisMaxParams.QF)[0]);
+    }
+    
+    @Test
+    public void testQueryFieldsTeachLangFi() {
+        
+        LearningOpportunityQuery q = new LearningOpportunityQuery(TERM, PREREQUISITE, CITIES, FF_TEACH_LANG_FI, LANG_FI, ONGOING, UPCOMING, START, ROWS, SORT, ORDER);
+        assertNotNull(q);
+        assertEquals(6, q.getFilterQueries().length);
+        String prerequisiteFQ = new StringBuilder("prerequisites:").append(PREREQUISITE).toString();
+        assertEquals(prerequisiteFQ, q.getFilterQueries()[0]);
+        String lopHomeplaceFQ = new StringBuilder("lopHomeplace:(")
+                .append(Joiner.on(" OR ").join(CITIES)).append(")").toString();
+        assertEquals(lopHomeplaceFQ, q.getFilterQueries()[1]);
+        assertEquals(TERM, q.getQuery().toString());
+        assertEquals("edismax", q.getParams("defType")[0]);
+        assertEquals(Joiner.on(" ").join(LearningOpportunityQuery.FIELDS_FI), q.getParams(DisMaxParams.QF)[0]);
+        
+    }
+    
+    @Test
+    public void testQueryFieldsTeachLangSv() {
+        
+        LearningOpportunityQuery q = new LearningOpportunityQuery(TERM, PREREQUISITE, CITIES, FF_TEACH_LANG_SV, LANG_FI, ONGOING, UPCOMING, START, ROWS, SORT, ORDER);
+        assertNotNull(q);
+        assertEquals(6, q.getFilterQueries().length);
+        String prerequisiteFQ = new StringBuilder("prerequisites:").append(PREREQUISITE).toString();
+        assertEquals(prerequisiteFQ, q.getFilterQueries()[0]);
+        String lopHomeplaceFQ = new StringBuilder("lopHomeplace:(")
+                .append(Joiner.on(" OR ").join(CITIES)).append(")").toString();
+        assertEquals(lopHomeplaceFQ, q.getFilterQueries()[1]);
+        assertEquals(TERM, q.getQuery().toString());
+        assertEquals("edismax", q.getParams("defType")[0]);
+        assertEquals(Joiner.on(" ").join(LearningOpportunityQuery.FIELDS_SV), q.getParams(DisMaxParams.QF)[0]);
+        
+    }
+    
+    @Test
+    public void testQueryFieldsTeachLangOther() {
+        
+        LearningOpportunityQuery q = new LearningOpportunityQuery(TERM, PREREQUISITE, CITIES, FF_TEACH_LANG_OTHER, LANG_FI, ONGOING, UPCOMING, START, ROWS, SORT, ORDER);
+        assertNotNull(q);
+        assertEquals(6, q.getFilterQueries().length);
+        String prerequisiteFQ = new StringBuilder("prerequisites:").append(PREREQUISITE).toString();
+        assertEquals(prerequisiteFQ, q.getFilterQueries()[0]);
+        String lopHomeplaceFQ = new StringBuilder("lopHomeplace:(")
+                .append(Joiner.on(" OR ").join(CITIES)).append(")").toString();
+        assertEquals(lopHomeplaceFQ, q.getFilterQueries()[1]);
+        assertEquals(TERM, q.getQuery().toString());
+        assertEquals("edismax", q.getParams("defType")[0]);
+        assertEquals(Joiner.on(" ").join(LearningOpportunityQuery.FIELDS_FI), q.getParams(DisMaxParams.QF)[0]);
+        
     }
     
 
