1586f1038caaf02581e2f5b794a8fdc2094c8977
mholi <markus.holi@proactum.fi>
2014-01-20 13:54:39 +0200

    KSH-261: kommentteja

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/dao/UniversityAppliedScienceLOSDAO.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/dao/UniversityAppliedScienceLOSDAO.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
 package fi.vm.sade.koulutusinformaatio.dao;
 
 import org.mongodb.morphia.Morphia;
@@ -7,6 +22,10 @@ import com.mongodb.Mongo;
 
 import fi.vm.sade.koulutusinformaatio.dao.entity.UniversityAppliedScienceLOSEntity;
 
+/**
+ * 
+ * @author Markus
+ */
 public class UniversityAppliedScienceLOSDAO extends BasicDAO<UniversityAppliedScienceLOSEntity, String> {
 	
     public UniversityAppliedScienceLOSDAO(Mongo mongo, Morphia morphia, String dbName) {
