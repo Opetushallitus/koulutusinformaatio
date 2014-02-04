383868eaf6b3837aeb162d356b20c09e5b168411
mholi <markus.holi@proactum.fi>
2014-02-04 11:25:13 +0200

    OVT-6726: perusfiksi

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/UpdateServiceImpl.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/UpdateServiceImpl.java
@@ -30,7 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -79,18 +78,12 @@ public class UpdateServiceImpl implements UpdateService {
             
             int count = MAX_RESULTS;
             int index = 0;
-            /*
+            
             while(count >= MAX_RESULTS) {
                 LOG.debug("Searching parent learning opportunity oids count: " + count + ", start index: " + index);
                 List<String> loOids = tarjontaService.listParentLearnignOpportunityOids(count, index);
                 count = loOids.size();
-                index += count;*/
-            
-                List<String> loOids = Arrays.asList("1.2.246.562.5.2013061010191208547980", 
-                        "1.2.246.562.5.2013061010192577322360", 
-                        "1.2.246.562.5.2013112814572435763432", 
-                        "1.2.246.562.5.2013061010184670694756");//,
-                		//"1.2.246.562.5.2013061010190108136320");
+                index += count;
             
                for (String loOid : loOids) {
                     List<LOS> specifications = null;
@@ -106,7 +99,7 @@ public class UpdateServiceImpl implements UpdateService {
                     }
                    this.indexerService.commitLOChanges(loUpdateSolr, lopUpdateSolr, locationUpdateSolr, false);
                }
-            //}
+            }
             List<Location> locations = locationService.getMunicipalities();
             indexerService.addLocations(locations, locationUpdateSolr);
             indexerService.commitLOChanges(loUpdateSolr, lopUpdateSolr, locationUpdateSolr, true);
