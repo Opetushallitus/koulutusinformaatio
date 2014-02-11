2d061ee07d2e38ac1ca87d49b84f73d7c07d2824
mholi <markus.holi@proactum.fi>
2014-02-11 12:10:54 +0200

    KSH-261: tests

---
+++ koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/converter/ParentLOSToSolrInputDocumentTest.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+package fi.vm.sade.koulutusinformaatio.converter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.Before;
+import org.junit.Test;
+
+import fi.vm.sade.koulutusinformaatio.converter.SolrUtil.LearningOpportunity;
+import fi.vm.sade.koulutusinformaatio.converter.SolrUtil.SolrConstants;
+import fi.vm.sade.koulutusinformaatio.domain.Address;
+import fi.vm.sade.koulutusinformaatio.domain.ApplicationOption;
+import fi.vm.sade.koulutusinformaatio.domain.ApplicationSystem;
+import fi.vm.sade.koulutusinformaatio.domain.ChildLOI;
+import fi.vm.sade.koulutusinformaatio.domain.ChildLOS;
+import fi.vm.sade.koulutusinformaatio.domain.Code;
+import fi.vm.sade.koulutusinformaatio.domain.DateRange;
+import fi.vm.sade.koulutusinformaatio.domain.ParentLOI;
+import fi.vm.sade.koulutusinformaatio.domain.ParentLOS;
+import fi.vm.sade.koulutusinformaatio.domain.Provider;
+import fi.vm.sade.koulutusinformaatio.util.TestUtil;
+
+/**
+ * 
+ * @author Markus
+ */
+public class ParentLOSToSolrInputDocumentTest {
+	
+	private ParentLOS los;
+	private ParentLOSToSolrInputDocument converter;
+	private Code prerequisite;
+	private Provider provider;
+	
+	@Before
+	public void setUp() {
+		los = new ParentLOS();
+		los.setType("parentLos");
+		los.setId("parentId");
+		los.setName(TestUtil.createI18nText("parent name fi", "parent name sv", "parent name en"));
+		
+		Code lang = new Code();
+		lang.setName(TestUtil.createI18nText("Suomi", "Finska", "Finnish"));
+		lang.setValue("FI");
+		lang.setUri("fi_uri");
+		los.setTeachingLanguages(Arrays.asList(lang));
+		
+		provider = new Provider();
+		provider.setId("provId");
+		provider.setName(TestUtil.createI18nText("prov fi", "prov sv", "prov en"));
+		provider.setHomeDistrict(TestUtil.createI18nText("Uusimaa fi", "Uusimaa sv", "Uusimaa en"));
+		provider.setHomePlace(TestUtil.createI18nText("Hki fi", "Hki sv", "Hki en"));
+		Address addr = new Address();
+		addr.setPostOffice("04620");
+		provider.setVisitingAddress(addr);
+		provider.setDescription(TestUtil.createI18nText("prov descr fi", "prov descr sv", "prov descr en"));
+		los.setProvider(provider);
+		
+		los.setCreditValue("80");
+		los.setCreditUnit(TestUtil.createI18nText("ov fi", "ov sv", "ov en"));
+		los.setGoals(TestUtil.createI18nText("Goals fi", "Goals sv", "Goals en"));
+		
+		ParentLOI parentLoi = new ParentLOI();
+		ApplicationSystem as = new ApplicationSystem();
+		as.setName(TestUtil.createI18nText("Haku fi", "Haku sv", "Haku en"));
+		Date asStart = new Date();
+		Date asEnd = new Date();
+		DateRange dr = new DateRange();
+		dr.setStartDate(asStart);
+		dr.setEndDate(asEnd);
+		as.setApplicationDates(Arrays.asList(dr));
+		ApplicationOption ao = new ApplicationOption();
+		ao.setSpecificApplicationDates(false);
+		ao.setApplicationSystem(as);
+		parentLoi.setApplicationOptions(new HashSet<ApplicationOption>(Arrays.asList(ao)));
+		los.setLois(Arrays.asList(parentLoi));
+		
+		List<Code> topics = new ArrayList<Code>();
+		Code topic1 = new Code();
+		topic1.setName(TestUtil.createI18nText("topiikii 1 fi", "topiikki 1 sv", "topiikki 1 en"));
+		topic1.setValue("top1");
+		topic1.setUri("top1_uri");
+		topics.add(topic1);
+		
+		Code topic2 = new Code();
+		topic2.setName(TestUtil.createI18nText("topiikii 2 fi", "topiikki 2 sv", "topiikki 2 en"));
+		topic2.setValue("top2");
+		topic2.setUri("top2_uri");
+		topics.add(topic2);
+		los.setTopics(topics);
+		
+		List<Code> themes = new ArrayList<Code>();
+		Code theme1 = new Code();
+		theme1.setName(TestUtil.createI18nText("theme 1 fi", "theme 1 sv", "theme 1 en"));
+		theme1.setValue("theme1");
+		theme1.setUri("theme1_uri");
+		themes.add(theme1);
+		
+		Code theme2 = new Code();
+		theme2.setName(TestUtil.createI18nText("theme 2 fi", "theme 2 sv", "theme 2 en"));
+		theme2.setValue("theme2");
+		theme2.setUri("theme2_uri");
+		themes.add(theme2);
+		los.setThemes(themes);
+		
+		ChildLOS childLos = new ChildLOS();
+		childLos.setName(TestUtil.createI18nText("child los name fi", "child los name sv", "child los name en"));
+		childLos.setQualification(TestUtil.createI18nText("quali fi", "quali sv", "quali en"));
+		childLos.setGoals(TestUtil.createI18nText("Goals child fi", "Goals child sv", "Goals child en"));
+		
+		ChildLOI childLoi = new ChildLOI();
+		childLoi.setStartDate(new Date());
+		
+		prerequisite = new Code();
+		prerequisite.setName(TestUtil.createI18nText("Peruskoulu", "Peruskoulu sv", "Peruskoulu en"));
+		prerequisite.setValue("pk");
+		prerequisite.setUri("pk_uri");
+		childLoi.setPrerequisite(prerequisite);
+		childLoi.setTeachingLanguages(Arrays.asList(lang));
+		childLoi.setProfessionalTitles(Arrays.asList(TestUtil.createI18nText("profession fi", "profession sv", "profession en")));
+		childLoi.setContent(TestUtil.createI18nText("Content fi", "Content sv", "Content en"));
+		childLoi.setApplicationOptions(Arrays.asList(ao));
+		childLoi.setKaksoistutkinto(false);
+		childLos.setLois(Arrays.asList(childLoi));
+		los.setChildren(Arrays.asList(childLos));
+		
+		converter = new ParentLOSToSolrInputDocument();
+	}
+	
+	@Test
+	public void testConvert() {
+		
+		List<SolrInputDocument> docs = converter.convert(los);
+		assertEquals(7, docs.size());
+		SolrInputDocument doc = docs.get(0);
+		assertEquals(los.getId(), doc.get(LearningOpportunity.ID).getValue().toString());
+		assertEquals(prerequisite.getValue(), doc.get(LearningOpportunity.PREREQUISITES).getValues().iterator().next().toString());
+		assertEquals("80 ov fi", doc.get(LearningOpportunity.CREDITS).getValue().toString());
+        assertEquals(provider.getName().getTranslations().get("fi"), doc.get(LearningOpportunity.LOP_NAME).getValue().toString());
+        assertEquals(SolrConstants.ED_TYPE_AMMATILLINEN, doc.get(LearningOpportunity.EDUCATION_TYPE).getValue().toString());
+		
+	}
+	
+	@Test
+	public void testConvertKaksoistutkinto() {
+		los.getChildren().get(0).getLois().get(0).setKaksoistutkinto(true);
+		List<SolrInputDocument> docs = converter.convert(los);
+		assertEquals(7, docs.size());
+		SolrInputDocument doc = docs.get(0);
+		assertEquals(los.getId(), doc.get(LearningOpportunity.ID).getValue().toString());
+		assertEquals(prerequisite.getValue(), doc.get(LearningOpportunity.PREREQUISITES).getValues().iterator().next().toString());
+		assertEquals("80 ov fi", doc.get(LearningOpportunity.CREDITS).getValue().toString());
+        assertEquals(provider.getName().getTranslations().get("fi"), doc.get(LearningOpportunity.LOP_NAME).getValue().toString());
+        assertTrue(doc.get(LearningOpportunity.EDUCATION_TYPE).getValue().toString().contains(SolrUtil.SolrConstants.ED_TYPE_KAKSOIS));
+	}
+
+}
