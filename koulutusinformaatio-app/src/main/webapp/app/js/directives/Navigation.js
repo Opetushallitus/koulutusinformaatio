44759dad05b1f1a7246983054a00887f30b33967
Kalle Lundahn <kalle.lundahn@proactum.fi>
2014-04-08 13:14:56 +0300

    KSH-312: Skip navigation -linkki lisätty

--- koulutusinformaatio-app/src/main/webapp/app/js/directives/Navigation.js
+++ koulutusinformaatio-app/src/main/webapp/app/js/directives/Navigation.js
@@ -15,6 +15,20 @@ directive('kiNavigation', function() {
     }
 }).
 
+directive('kiSkipNavigation', function() {
+    return {
+        restrict: 'A',
+        template: '<div id="skip-link"><a href="javascript:void(0)" data-ki-i18n="skip-to-content" data-ng-click="skipNavigation()"></a></div>',
+        controller: function($scope, $location, $anchorScroll) {
+            $scope.skipNavigation = function() {
+                $location.hash('page');
+                $anchorScroll();
+                angular.element('#search-field').focus();
+            }
+        }
+    }
+}).
+
 directive('tree', function ($compile) {
 return {
     restrict: 'A',
