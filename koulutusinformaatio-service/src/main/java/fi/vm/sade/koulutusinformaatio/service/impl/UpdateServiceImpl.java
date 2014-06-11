5d4a036e0cc214226e4d4f979b368f7ef35e383a
mholi <markus.holi@proactum.fi>
2014-06-11 13:00:49 +0200

    OVT-7994: korjauksia

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/UpdateServiceImpl.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/UpdateServiceImpl.java
@@ -16,7 +16,6 @@
 
 package fi.vm.sade.koulutusinformaatio.service.impl;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -95,19 +94,12 @@ public class UpdateServiceImpl implements UpdateService {
             int count = MAX_RESULTS;
             int index = 0;
 
-            /*while (count >= MAX_RESULTS) {
+            while (count >= MAX_RESULTS) {
             LOG.debug("Searching parent learning opportunity oids count: " + count + ", start index: " + index);
             List<String> loOids = tarjontaService.listParentLearnignOpportunityOids(count, index);
             count = loOids.size();
-            index += count;*/
-            List<String> loOids = Arrays.asList(
-                    "1.2.246.562.5.2013061010191617423298",
-                    "1.2.246.562.5.2013061010184614853416", //kotitalous
-                    "1.2.246.562.5.2013061010184443434255", //amm
-                    "1.2.246.562.5.2013061010191530269331", //lukio
-                    "1.2.246.562.5.2013112814572429147350", //valmistava
-                    "1.2.246.562.5.2013061010184317101998" //amm kuvataide
-                    );
+            index += count;
+           
                 for (String loOid : loOids) {
                     List<LOS> specifications = null;
                     try {
@@ -122,7 +114,7 @@ public class UpdateServiceImpl implements UpdateService {
                         this.educationDataUpdateService.save(spec);
                     }
                 }
-            //}
+            }
 
             List<HigherEducationLOS> higherEducations = this.tarjontaService.findHigherEducations();
             LOG.debug("Found higher educations: " + higherEducations.size());
