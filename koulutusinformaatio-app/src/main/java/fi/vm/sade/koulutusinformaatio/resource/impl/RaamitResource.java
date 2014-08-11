04b7187a2cc6e7796c249db95bc716d4da7db69f
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-08-11 14:44:58 +0300

    Use oppija-raamit location from config

---
+++ koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/resource/impl/RaamitResource.java
@@ -0,0 +1,41 @@
+package fi.vm.sade.koulutusinformaatio.resource.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URISyntaxException;
+
+@Component
+@Path("/raamit")
+public class RaamitResource {
+
+    private String raamitLocation;
+
+    @Autowired
+    public RaamitResource(@Value("${oppija-raamit.url}") String raamitLocation) {
+        this.raamitLocation = raamitLocation;
+    }
+
+    @GET
+    @Path("/load")
+    public Response load() throws URISyntaxException {
+
+        StringBuilder b = new StringBuilder()
+                .append("if (document.location.hash.indexOf(\"skipRaamit\") < 0) {\n")
+                .append("    var raamit = document.getElementById(\"apply-raamit\")\n")
+                .append("    if(!raamit) {\n")
+                .append("        raamit = document.createElement(\"script\")\n")
+                .append("        raamit.id = \"apply-raamit\"\n")
+                .append("        raamit.src = \"").append(raamitLocation).append("/oppija-raamit/apply-raamit.js\"\n")
+                .append("        document.getElementsByTagName(\"head\")[0].appendChild(raamit)")
+                .append("    }\n")
+                .append("}\n");
+
+        return Response.ok(b.toString(), MediaType.APPLICATION_JSON).build();
+    }
+}
