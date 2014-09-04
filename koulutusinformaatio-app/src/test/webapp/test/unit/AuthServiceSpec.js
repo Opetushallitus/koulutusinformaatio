e758497c08ec87e1c44b79a9f98760f664558622
Kalle Lundahn <kalle.lundahn@proactum.fi>
2014-09-04 15:58:54 +0300

    Unit tests added

---
+++ koulutusinformaatio-app/src/test/webapp/test/unit/AuthServiceSpec.js
@@ -0,0 +1,27 @@
+describe('AuthService', function() {
+    var utility, cookie;
+    var cookieName = "auth";
+
+    beforeEach(function() {
+        module('kiApp', 'kiApp.AuthService', 'kiApp.CookieService');
+
+        inject(function(AuthService, CookieService) {
+            utility = AuthService;
+            cookie = CookieService;
+        });
+
+        // delete cookie
+        cookie.set(cookieName, null, {}, false);
+    });
+
+    it('should return false for unset auth cookie', function() {
+        var result = utility.isAuthenticated();
+        expect(result).toBeFalsy();
+    });
+
+    it('should return true for set auth cookie', function() {
+        cookie.set(cookieName, 'authCookieSet', {}, false);
+        var result = utility.isAuthenticated();
+        expect(result).toBeTruthy();
+    });
+});
