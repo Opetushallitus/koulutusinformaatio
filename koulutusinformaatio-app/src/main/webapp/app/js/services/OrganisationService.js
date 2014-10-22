e714958d3732c4f5c98ba2098c758508064eac49
Kalle Lundahn <kalle.lundahn@proactum.fi>
2014-10-22 10:13:28 +0300

    KSH-373: pass UI language param for organisation search

--- koulutusinformaatio-app/src/main/webapp/app/js/services/OrganisationService.js
+++ koulutusinformaatio-app/src/main/webapp/app/js/services/OrganisationService.js
@@ -3,13 +3,17 @@ angular.module('kiApp.OrganisationService', ['ngResource']).
 /**
  * Service for retrieving districts (maakunnat). Used in faceted search
  */
-service('OrganisationService', ['$http', '$timeout', '$q', function($http, $timeout, $q) {
+service('OrganisationService', ['$http', '$timeout', '$q', 'LanguageService', function($http, $timeout, $q, LanguageService) {
 
     return {
         query: function(id) {
             var deferred = $q.defer();
 
-            $http.get('../lop/' + id).
+            $http.get('../lop/' + id, {
+                params: {
+                    lang: LanguageService.getLanguage()
+                }
+            }).
             success(function(result) {
                 deferred.resolve(result);
             }).
