2788be2dcfc36cb2813bf58c030e27a96e837faa
Humakt83 <risto.salama@cybercom.com>
2015-02-03 15:59:34 +0200

    Updated constant education_domain
    It shares similar purpose to education code, so added ssort postfix to
    it

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/converter/SolrUtil.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/converter/SolrUtil.java
@@ -352,7 +352,7 @@ public final class SolrUtil {
         
         public static final String EDUCATION_TYPE_DISPLAY = "educationCode_en_ss";
         
-        public static final String EDUCATION_DOMAIN = "education_domain";
+        public static final String EDUCATION_DOMAIN = "education_domain_ssort";
         
         public static final String ARTICLE_URL = "article_url_ss";
         public static final String ARTICLE_PICTURE = "article_picture_ss";
