31fc3901f0ed6bb8c1582f1ea1f90f2fce266428
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2015-02-13 14:18:32 +0200

    Revert "BUG-37: Lisätty KIlle ensisijainen kielen päättely domainin perusteella."
    This reverts commit 4fbcb592b9abcf3cc004ad6e55252d3e1c2de125.

--- koulutusinformaatio-app/src/main/webapp/app/js/services/LanguageService.js
+++ koulutusinformaatio-app/src/main/webapp/app/js/services/LanguageService.js
@@ -13,22 +13,9 @@ service('LanguageService', ['CookieService', '$location', '_', function(CookieSe
         defaultLanguage = languages.finnish,
         key = 'i18next',
 
-        getLanguageFromHost = function(host) {
-            var x = host.split('.')
-            if (x.length < 2)
-                return null
-            switch (x[x.length - 2]) {
-            case 'opintopolku': return 'fi'
-            case 'studieinfo': return 'sv'
-            case 'studyinfo': return 'en'
-            }
-            return null
-        }
-
         // initialize app language with value from cookie
-        currentLanguage = getLanguageFromHost(window.location.host) || CookieService.get(key) || defaultLanguage,
+        currentLanguage = CookieService.get(key) || defaultLanguage,
 
-        
         getLanguage = function() {
             return currentLanguage;
         },
