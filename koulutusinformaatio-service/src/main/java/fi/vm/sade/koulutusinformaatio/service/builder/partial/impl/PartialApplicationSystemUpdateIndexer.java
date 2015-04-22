f9f975a6deb14dfd547fa99a0122679186f8cc73
Humakt83 <risto.salama@cybercom.com>
2015-04-22 15:03:02 +0300

    KSH-710: Service for updating application system
    And associated options. Uses incremental indexer classes.

---
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/builder/partial/impl/PartialApplicationSystemUpdateIndexer.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2015 The Finnish National Board of Education - Opetushallitus
+ *
+ * This program is free software: Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * European Union Public Licence for more details.
+ */
+package fi.vm.sade.koulutusinformaatio.service.builder.partial.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fi.vm.sade.koulutusinformaatio.service.TarjontaRawService;
+import fi.vm.sade.koulutusinformaatio.service.builder.impl.incremental.IncrementalApplicationOptionIndexer;
+import fi.vm.sade.koulutusinformaatio.service.builder.impl.incremental.IncrementalApplicationSystemIndexer;
+import fi.vm.sade.koulutusinformaatio.service.builder.impl.incremental.IncrementalLOSIndexer;
+import fi.vm.sade.koulutusinformaatio.service.builder.partial.PartialUpdateIndexer;
+import fi.vm.sade.tarjonta.service.resources.dto.HakuDTO;
+import fi.vm.sade.tarjonta.service.resources.dto.OidRDTO;
+
+/**
+ * @author risal1
+ *
+ */
+@Component
+public class PartialApplicationSystemUpdateIndexer implements PartialUpdateIndexer {
+    
+    private static Logger LOGGER = LoggerFactory.getLogger(PartialApplicationSystemUpdateIndexer.class);
+    
+    @Autowired
+    private TarjontaRawService tarjontaService;
+    
+    @Autowired
+    private IncrementalApplicationSystemIndexer asIndexer; 
+    
+    @Autowired
+    private IncrementalApplicationOptionIndexer aoIndexer;
+    
+    @Override
+    public void update(String oid) throws Exception {        
+        asIndexer.indexApplicationSystemData(oid);
+        HakuDTO hakuDto = tarjontaService.getHaku(oid);
+        for (OidRDTO hakuKohde : tarjontaService.getHakukohdesByHaku(oid)) {
+            updateApplicationOptions(hakuDto, hakuKohde.getOid());
+        }        
+    }
+
+    private void updateApplicationOptions(HakuDTO hakuDto, String hakuKohdeOid) throws Exception {
+        LOGGER.debug("Indexing + application option: " + hakuKohdeOid);
+        aoIndexer.indexApplicationOptionData(tarjontaService.getHakukohde(hakuKohdeOid), hakuDto);
+    }
+
+}
---
+++ koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/service/builder/partial/impl/PartialApplicationSystemUpdateIndexerTest.java
@@ -0,0 +1,70 @@
+package fi.vm.sade.koulutusinformaatio.service.builder.partial.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+import fi.vm.sade.koulutusinformaatio.service.TarjontaRawService;
+import fi.vm.sade.koulutusinformaatio.service.builder.impl.incremental.IncrementalApplicationOptionIndexer;
+import fi.vm.sade.koulutusinformaatio.service.builder.impl.incremental.IncrementalApplicationSystemIndexer;
+import fi.vm.sade.tarjonta.service.resources.dto.HakuDTO;
+import fi.vm.sade.tarjonta.service.resources.dto.HakukohdeDTO;
+import fi.vm.sade.tarjonta.service.resources.dto.OidRDTO;
+import static org.mockito.Matchers.any;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PartialApplicationSystemUpdateIndexerTest {
+
+    private final static String APPLICATION_OID = "121.21321.41.2312";
+    
+    private final static List<String> ASSOCIATED_APPLICATION_OPTION_OIDS = Arrays.asList("123.12", "414.123", "345.2.3");
+    
+    @Mock
+    private TarjontaRawService tarjontaService;
+    
+    @Mock
+    private IncrementalApplicationSystemIndexer asIndexer; 
+    
+    @Mock
+    private IncrementalApplicationOptionIndexer aoIndexer;
+    
+    @InjectMocks
+    private PartialApplicationSystemUpdateIndexer indexer;
+
+    @Test
+    public void usesTarjontaServiceToFetchEducation() throws Exception {
+        indexer.update(APPLICATION_OID);
+        verify(tarjontaService).getHaku(APPLICATION_OID);
+    }
+    
+    @Test
+    public void updatesAllApplicationOptionsAssociatedWithSystem() throws Exception {        
+        when(tarjontaService.getHakukohdesByHaku(APPLICATION_OID)).thenReturn(givenApplicationOptionsResponse());
+        indexer.update(APPLICATION_OID);
+        verify(aoIndexer, times(ASSOCIATED_APPLICATION_OPTION_OIDS.size())).indexApplicationOptionData(any(HakukohdeDTO.class), any(HakuDTO.class));
+    }
+
+    private List<OidRDTO> givenApplicationOptionsResponse() {
+        return Lists.transform(ASSOCIATED_APPLICATION_OPTION_OIDS, new Function<String, OidRDTO>() {
+
+            @Override
+            public OidRDTO apply(String input) {
+                return new OidRDTO(input);
+            }
+            
+        });
+    }
+    
+}
