ccf0eb446cb01bbddf54aee9026263d82f1bfb2e
Humakt83 <urlact@yahoo.com>
2015-04-30 11:59:20 +0300

    Revert "KSH-701: Vapaan sivistystyön fasetit"
    This reverts commit c313ce9b72ca70f9af2ce85f76a1146f8af7228b.

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/converter/HigherEducationLOSToSolrInputDocment.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/converter/HigherEducationLOSToSolrInputDocment.java
@@ -577,9 +577,6 @@ public class HigherEducationLOSToSolrInputDocment implements Converter<Standalon
             } else if (los.getEducationType().equals(SolrConstants.ED_TYPE_VALMENTAVA)) {
                 doc.addField(LearningOpportunity.EDUCATION_TYPE, SolrUtil.SolrConstants.ED_TYPE_MUU);
                 doc.addField(LearningOpportunity.EDUCATION_TYPE, SolrUtil.SolrConstants.ED_TYPE_VALMENTAVA);
-            } else if (los.getEducationType().equals(SolrConstants.ED_TYPE_KANSANOPISTO)) {
-                doc.addField(LearningOpportunity.EDUCATION_TYPE, SolrUtil.SolrConstants.ED_TYPE_MUU);
-                doc.addField(LearningOpportunity.EDUCATION_TYPE, SolrUtil.SolrConstants.ED_TYPE_KANSANOPISTO);
             } else if (los.getEducationType().equals(SolrConstants.ED_TYPE_TENTH_GRADE)) {
                 doc.addField(LearningOpportunity.EDUCATION_TYPE, SolrUtil.SolrConstants.ED_TYPE_MUU);
                 doc.addField(LearningOpportunity.EDUCATION_TYPE, SolrUtil.SolrConstants.ED_TYPE_PK_JALK);
