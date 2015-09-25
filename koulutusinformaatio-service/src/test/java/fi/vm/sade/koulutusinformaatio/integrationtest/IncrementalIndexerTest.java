6237118383fde3dfaf9fc06f19c78feaec1aa999
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2015-09-25 17:51:57 +0300

    BUG-521: Testiin lisätty provider ja koulutusten poistamiseen vaadittavat kentät.

--- koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/integrationtest/IncrementalIndexerTest.java
+++ koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/integrationtest/IncrementalIndexerTest.java
@@ -1,9 +1,12 @@
 package fi.vm.sade.koulutusinformaatio.integrationtest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.After;
 import org.junit.Test;
@@ -12,9 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import fi.vm.sade.koulutusinformaatio.dao.ApplicationOptionDAO;
 import fi.vm.sade.koulutusinformaatio.dao.KoulutusLOSDAO;
+import fi.vm.sade.koulutusinformaatio.dao.LearningOpportunityProviderDAO;
 import fi.vm.sade.koulutusinformaatio.dao.TutkintoLOSDAO;
+import fi.vm.sade.koulutusinformaatio.dao.entity.ApplicationOptionEntity;
+import fi.vm.sade.koulutusinformaatio.dao.entity.CodeEntity;
 import fi.vm.sade.koulutusinformaatio.dao.entity.KoulutusLOSEntity;
+import fi.vm.sade.koulutusinformaatio.dao.entity.LearningOpportunityProviderEntity;
 import fi.vm.sade.koulutusinformaatio.dao.entity.TutkintoLOSEntity;
 import fi.vm.sade.koulutusinformaatio.service.IncrementalUpdateService;
 import fi.vm.sade.tarjonta.shared.types.ToteutustyyppiEnum;
@@ -30,7 +38,13 @@ public class IncrementalIndexerTest {
     private KoulutusLOSDAO koulutusLOSDAO;
 
     @Autowired
+    private ApplicationOptionDAO applicationOptionDAO;
+
+    @Autowired
     private TutkintoLOSDAO tutkintoLOSDAO;
+
+    @Autowired
+    private LearningOpportunityProviderDAO learningOpportunityProviderDAO;
 
     @Autowired
     private TestHelper testHelper;
@@ -58,22 +72,29 @@ public class IncrementalIndexerTest {
     @Test
     public void testBug521() throws Exception {
         tarjontaRawServiceMock.setTestCase("testBug521");
+
+        LearningOpportunityProviderEntity provider = new LearningOpportunityProviderEntity();
+        provider.setId("1.2.246.562.10.10779357598");
         
         TutkintoLOSEntity tutkinto1 = new TutkintoLOSEntity();
         tutkinto1.setId("1.2.246.562.5.2013061010184190024479_1.2.246.562.10.10779357598_2015_Syksy");
-        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.61512976061", tutkinto1));
-        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.27586825005", tutkinto1));
-        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.165818280810", tutkinto1));
-        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.25298814663", tutkinto1));
+        tutkinto1.setProvider(provider);
+        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.61512976061", tutkinto1, "koulutus_321101", provider));
+        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.27586825005", tutkinto1, "koulutus_321101", provider));
+        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.165818280810", tutkinto1, "koulutus_321101", provider));
+        tutkinto1.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.25298814663", tutkinto1, "koulutus_321101", provider));
         
         TutkintoLOSEntity tutkinto2 = new TutkintoLOSEntity();
         tutkinto2.setId("1.2.246.562.5.2013061010184190024479_1.2.246.562.10.10779357598_2016_Kevät");
-        tutkinto2.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.61486530712", tutkinto2));
-        tutkinto2.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.64285210601", tutkinto2));
+        tutkinto2.setProvider(provider);
+        tutkinto2.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.61486530712", tutkinto2, "koulutus_321101", provider));
+        tutkinto2.getChildEducations().add(getkoulutusLosEntity("1.2.246.562.17.64285210601", tutkinto2, "koulutus_321101", provider));
 
         tutkintoLOSDAO.save(tutkinto1);
         tutkintoLOSDAO.save(tutkinto2);
         
+        learningOpportunityProviderDAO.save(provider);
+
         for (KoulutusLOSEntity los : tutkinto1.getChildEducations()) {
             koulutusLOSDAO.save(los);
         }
@@ -83,19 +104,68 @@ public class IncrementalIndexerTest {
 
         assertEquals(2, tutkintoLOSDAO.count());
         assertEquals(6, koulutusLOSDAO.count());
+        assertEquals(0, applicationOptionDAO.count());
 
         incrementalUpdateService.updateChangedEducationData();
 
-        assertFalse("Tee testi loppuun", true);
+        List<TutkintoLOSEntity> tutkintos = tutkintoLOSDAO.find().asList();
+        List<KoulutusLOSEntity> losses = koulutusLOSDAO.find().asList();
+        List<ApplicationOptionEntity> aos = applicationOptionDAO.find().asList();
+        assertEquals(2, tutkintos.size());
+        assertEquals(6, losses.size());
+        assertEquals(6, aos.size());
+
+        Set<String> usedAoOids = new HashSet<String>();
+        Set<String> usedKoulutusOids = new HashSet<String>();
+
+        for (TutkintoLOSEntity tutkinto : tutkintos) {
+            validateTutkinto(tutkinto);
+            for (KoulutusLOSEntity koulutus : tutkinto.getChildEducations()) {
+                usedKoulutusOids.add(koulutus.getId());
+            }
+            for (ApplicationOptionEntity ao : tutkinto.getApplicationOptions()) {
+                usedAoOids.add(ao.getId());
+            }
+        }
+        for (KoulutusLOSEntity koulutus : losses) {
+            validateKoulutus(koulutus);
+            usedKoulutusOids.remove(koulutus.getId());
+            for (ApplicationOptionEntity ao : koulutus.getApplicationOptions()) {
+                usedAoOids.add(ao.getId());
+            }
+        }
+        for (ApplicationOptionEntity ao : aos) {
+            validateAo(ao);
+            usedAoOids.remove(ao.getId());
+        }
+        assertEquals("There were orphaned koulutus: " + usedKoulutusOids, 0, usedKoulutusOids.size());
+        assertEquals("There were orphaned aos: " + usedAoOids, 0, usedAoOids.size());
     }
 
-    private KoulutusLOSEntity getkoulutusLosEntity(String oid, TutkintoLOSEntity tutkinto) {
+    private void validateTutkinto(TutkintoLOSEntity tutkinto) {
+        assertTrue("Tutkinto " + tutkinto.getId() + " has no application options.", tutkinto.getApplicationOptions().size() > 0);
+        assertTrue("Tutkinto " + tutkinto.getId() + " has no koulutus.", tutkinto.getChildEducations().size() > 0);
+    }
+
+    private void validateKoulutus(KoulutusLOSEntity koulutus) {
+        assertTrue("Koulutus " + koulutus.getId() + " has no application options.", koulutus.getApplicationOptions().size() > 0);
+        assertNotNull("Koulutus " + koulutus.getId() + " has no tutkinto.", koulutus.getTutkinto());
+    }
+
+    private void validateAo(ApplicationOptionEntity ao) {
+        assertNotNull(ao.getApplicationSystem());
+    }
+
+    private KoulutusLOSEntity getkoulutusLosEntity(String oid, TutkintoLOSEntity tutkinto, String koulutuskoodi, LearningOpportunityProviderEntity provider) {
         KoulutusLOSEntity e = new KoulutusLOSEntity();
         e.setId(oid);
         e.setTutkinto(tutkinto);
+        CodeEntity koulutusPrerequisite = new CodeEntity();
+        koulutusPrerequisite.setUri(koulutuskoodi);
+        e.setEducationCode(koulutusPrerequisite);
         e.setToteutustyyppi(ToteutustyyppiEnum.AMMATILLINEN_PERUSTUTKINTO);
+        e.setProvider(provider);
         return e;
-
     }
 
 }
