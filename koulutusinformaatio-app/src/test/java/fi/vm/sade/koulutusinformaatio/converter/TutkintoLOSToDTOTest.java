2c1afc2bac4ff9e7f989b40203be943beff5ff5d
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2015-09-29 15:28:38 +0300

    BUG-538: Rajapinnasta palautetaan virhe jos tutkinnolla ei ole pohjakoulutusta vastaavia koulutuksia.

---
+++ koulutusinformaatio-app/src/test/java/fi/vm/sade/koulutusinformaatio/converter/TutkintoLOSToDTOTest.java
@@ -0,0 +1,39 @@
+package fi.vm.sade.koulutusinformaatio.converter;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import fi.vm.sade.koulutusinformaatio.domain.Code;
+import fi.vm.sade.koulutusinformaatio.domain.KoulutusLOS;
+import fi.vm.sade.koulutusinformaatio.domain.TutkintoLOS;
+import fi.vm.sade.koulutusinformaatio.domain.dto.ParentLearningOpportunitySpecificationDTO;
+import fi.vm.sade.koulutusinformaatio.domain.exception.KIConversionException;
+
+public class TutkintoLOSToDTOTest {
+
+    @Test(expected = KIConversionException.class)
+    public void testThrowsExceptionWhenPrerequisiteDoesNotMatchChildren() {
+        TutkintoLOS tutkinto = new TutkintoLOS();
+        KoulutusLOS child = new KoulutusLOS();
+        Code code = new Code();
+        code.setValue("YO");
+        child.setKoulutusPrerequisite(code);
+        tutkinto.setChildEducations(Lists.newArrayList(child));
+        TutkintoLOSToDTO.convert(tutkinto, "fi", "fi", "fi", "PK");
+    }
+
+    @Test
+    public void testConvertsChildLOS() {
+        TutkintoLOS tutkinto = new TutkintoLOS();
+        KoulutusLOS child = new KoulutusLOS();
+        Code code = new Code();
+        code.setValue("PK");
+        child.setKoulutusPrerequisite(code);
+        tutkinto.setChildEducations(Lists.newArrayList(child));
+        ParentLearningOpportunitySpecificationDTO dto = TutkintoLOSToDTO.convert(tutkinto, "fi", "fi", "fi", "PK");
+        assertEquals(1, dto.getChildren().size());
+    }
+}
