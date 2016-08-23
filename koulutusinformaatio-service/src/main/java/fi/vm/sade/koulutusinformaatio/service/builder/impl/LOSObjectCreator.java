ef5b680f1edff4dc11606e7b6908e673e746de5f
Heikki Honkanen <heikki.honkanen@gofore.com>
2016-08-23 16:25:31 +0300

    Add check to make sure opintojaksos are not processed twice.

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/builder/impl/LOSObjectCreator.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/builder/impl/LOSObjectCreator.java
@@ -1345,6 +1345,8 @@ public class LOSObjectCreator extends ObjectCreator {
     private Set<String> alreadyCreatedKorkeakouluOpintos = Sets.newHashSet();
 
     public List<KoulutusLOS> createKorkeakouluOpintos(KorkeakouluOpintoV1RDTO dto, boolean checkStatus) {
+        if (alreadyCreatedKorkeakouluOpintos.contains(dto.getOid())) return Lists.newArrayList();
+
         HashMap<String, KoulutusLOS> createdOpintos = Maps.newHashMap();
         HashMap<String, Set<String>> childOids = Maps.newHashMap();
         createKorkeakouluopinto(dto, checkStatus, createdOpintos, childOids);
