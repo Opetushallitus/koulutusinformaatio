4a8ece2e8f89d15db44fbed3563eb007d5955cd6
Mikko Apo <mikko.apo@reaktor.fi>
2016-08-30 15:42:04 +0300

    oph-properties: fixed karma tests

--- koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/resource/impl/FrontPropertiesResource.java
+++ koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/resource/impl/FrontPropertiesResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/rest/frontProperties")
+@Path("/frontProperties.js")
 @Component
 public class FrontPropertiesResource {
 
