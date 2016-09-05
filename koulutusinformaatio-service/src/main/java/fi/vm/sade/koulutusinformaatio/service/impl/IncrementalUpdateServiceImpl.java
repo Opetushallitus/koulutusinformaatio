2695c053c609bf4ed1510e77c57c744d0e6f6b83
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2016-09-05 10:13:56 +0300

    Ei logiteta inkrementaalisen skippausta joka minuutti.

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/IncrementalUpdateServiceImpl.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/IncrementalUpdateServiceImpl.java
@@ -121,7 +121,7 @@ public class IncrementalUpdateServiceImpl implements IncrementalUpdateService {
             isRunning = true;
             Map<String, List<String>> result = listChangedLearningOpportunities(updatePeriod);
             if (!hasChanges(result)) {
-                LOG.info("No incremental changes. Stopping now.");
+                LOG.debug("No incremental changes. Stopping now.");
                 isRunning = false;
                 runningSince = 0;
                 return;
