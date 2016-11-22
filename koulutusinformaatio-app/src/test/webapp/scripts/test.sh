defa0a696550441a64185b7e32f903446d4f76c9
Janne Sorvari <janne.sorvari@reaktor.fi>
2016-11-22 10:41:08 +0200

    Update Karma tests

--- koulutusinformaatio-app/src/test/webapp/scripts/test.sh
+++ koulutusinformaatio-app/src/test/webapp/scripts/test.sh
@@ -6,5 +6,6 @@ echo ""
 echo "Starting Karma Server (https://github.com/karma-runner/karma/)"
 echo "-------------------------------------------------------------------"
 
+rm -rf node_modules/
 npm install
 node_modules/karma/bin/karma start $BASE_DIR/../config/testacular.conf.js $*
