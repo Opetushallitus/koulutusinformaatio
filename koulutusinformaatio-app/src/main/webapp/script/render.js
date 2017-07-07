e83b1fbe561ed5d3d9130d5307815c6a3cd43f9f
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-07-07 13:09:22 +0300

    SEO ignores css and images. Also logs failures.

--- koulutusinformaatio-app/src/main/webapp/script/render.js
+++ koulutusinformaatio-app/src/main/webapp/script/render.js
@@ -2,10 +2,11 @@ var page = require("webpage").create(),
     fs = require("fs"),
     system = require("system"),
     scriptTagRegex = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
+    cssTagRegex = /<link(.*?).css(.*?)>/gi,
     url, filename;
 
 var stripScriptTags = function(html) {
-    return html.replace(scriptTagRegex, '');
+    return html.replace(scriptTagRegex, '').replace(cssTagRegex, '');
 };
 
 if (system.args.length < 3) {
@@ -16,7 +17,24 @@ if (system.args.length < 3) {
 url = system.args[1];
 filename = system.args[2];
 
-page.open(url, function() {
+page.settings.loadImages = false;
+
+page.onResourceRequested = function(request) {
+//    console.log('Request ' + JSON.stringify(request, undefined, 4));
+};
+page.onResourceReceived = function(response) {
+//    console.log('Receive ' + JSON.stringify(response, undefined, 4));
+};
+page.onConsoleMessage = function(msg) {
+//    console.log(msg);
+};
+
+page.open(url, function(status) {
+
+    if (status === 'fail') {
+        console.log("Failed url " + url);
+        phantom.exit();
+    }
 
     var writeHtmlToFile = function() {
         var html = stripScriptTags(page.content);
@@ -38,4 +56,3 @@ page.open(url, function() {
         }
     }, 500);
 });
-
