fd8ce44e1b6bd31c963f27828142507371caa8f8
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-07-18 10:06:25 +0300

    BUG-1488: Added safe binding of html if tarjonta data is broken.

--- koulutusinformaatio-app/src/main/webapp/app/js/directives/directives.js
+++ koulutusinformaatio-app/src/main/webapp/app/js/directives/directives.js
@@ -625,4 +625,32 @@ directive('setFocusHere', function(){
                 element[0].focus();
             }
         };
-});
+}).
+/**
+ * Kuvaukseen voi olla syötettynä rikkinäistä html:ää. Tällöin näytetään teksti rikkinäisenä.
+ */
+// https://github.com/shaunbowe/ngBindHtmlIfSafe
+directive("bindHtmlIfSafe", ['$compile', '$sce', function ($compile, $sce) {
+    return function (scope, element, attrs) {
+        scope.$watch(
+            function (scope) {
+                return scope.$eval(attrs.ngBindHtmlIfSafe);
+            },
+            function (value) {
+                var sanitizedHtml = null;
+                try {
+                    sanitizedHtml = $sce.getTrustedHtml(value);
+                } catch (ex) {
+                }
+
+                if (sanitizedHtml != null) {
+                    element.html(sanitizedHtml);
+                } else {
+                    element.text(value);
+                }
+
+                $compile(element.contents())(scope);
+            }
+        );
+    }
+}]);
