cfc52e245a86941fc22f1e429fcc21c12a2dff02
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-09-13 13:14:27 +0300

    missing path param

--- koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/resource/impl/SnapshotResource.java
+++ koulutusinformaatio-app/src/main/java/fi/vm/sade/koulutusinformaatio/resource/impl/SnapshotResource.java
@@ -28,7 +28,7 @@ public class SnapshotResource {
     @Path("/{oid}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
-    public Response getSnapshotContent(final String oid) {
+    public Response getSnapshotContent(@PathParam("oid") String oid) {
         try {
             SnapshotDTO snapshot = seoSnapshotService.getSnapshot(oid);
             if (snapshot == null) {
@@ -55,3 +55,38 @@ public class SnapshotResource {
         }
     }
 }
+
+
+
+/*
+*
+*
+*
+*
+*
+@Path("/snapshot")
+@Api(value = "/snapshot", description = "Hakukohteiden hakukoneoptimoidun HTML-muotoisen kuvauksen tallennus ja haku")
+public interface SnapshotResource {
+    @GET
+    @Path("/{oid}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_HTML)
+    @ApiOperation(value = "Hakukohteen hakukoneoptimoidun HTML-muotoisen kuvauksen haku oid:llä",
+            notes = "",
+            response = String.class)
+    @ApiResponses({@ApiResponse(code = 200, message = "Haettu HTML-muotoinen kuvaus palautettu kutsujalle onnistuneesti"),
+            @ApiResponse(code = 404, message = "Haettua HTML-muotoista kuvuasta ei löytynyt")})
+    Response getSnapshotContent(@ApiParam(value = "Hakukohteen oid, josta HTML-muotoinen kuvaus on generoitu") @PathParam("oid") String oid);
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Tallentaa hakukohteen hakukoneoptimoidun HTML-kuvauksen tietokantaan",
+            notes = "",
+            response = SnapshotDTO.class)
+    @ApiResponse(code = 201, message = "HTML-muotoisen kuvauksen tallennus onnistui")
+    Response createSnapshot(@ApiParam("Hakukohteen HTML-muotoinen kuvaus") SnapshotDTO snapshot);
+
+
+*/
