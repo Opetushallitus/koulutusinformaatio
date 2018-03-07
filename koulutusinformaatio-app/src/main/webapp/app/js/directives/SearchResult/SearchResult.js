bf72adfb40867561917069047c40a9520ef23e78
Joni Mäkelä <joni.makela@gmail.com>
2018-03-07 10:03:47 +0200

    Added null check

--- koulutusinformaatio-app/src/main/webapp/app/js/directives/SearchResult/SearchResult.js
+++ koulutusinformaatio-app/src/main/webapp/app/js/directives/SearchResult/SearchResult.js
@@ -148,22 +148,24 @@ directive('extendedSearchresultData',
                     }
                     $scope.extendedLO = result;
 
-                    $scope.extendedLO.children.forEach(function(child){
-                        console.log("child id = " + child.id);
-                        for(var key in child.aoToRequiredBaseEdCode) {
-                            if(child.aoToRequiredBaseEdCode.hasOwnProperty(key)) {
-                                console.log("AO id = " + key);
-                                var list = $scope.AORequiredBaseEds[key];
-                                if(!list) {
-                                    list = [];
+                    if(result.children) {
+                        result.children.forEach(function (child) {
+                            console.log("child id = " + child.id);
+                            for (var key in child.aoToRequiredBaseEdCode) {
+                                if (child.aoToRequiredBaseEdCode.hasOwnProperty(key)) {
+                                    console.log("AO id = " + key);
+                                    var list = $scope.AORequiredBaseEds[key];
+                                    if (!list) {
+                                        list = [];
+                                    }
+                                    var requirements = child.aoToRequiredBaseEdCode[key];
+                                    $scope.loChildRequiredBaseEds[child.id] = requirements;
+                                    list = requirements;
+                                    $scope.AORequiredBaseEds[key] = list;
                                 }
-                                var requirements = child.aoToRequiredBaseEdCode[key];
-                                $scope.loChildRequiredBaseEds[child.id] = requirements;
-                                list  = requirements;
-                                $scope.AORequiredBaseEds[key] = list;
                             }
-                        }
-                    });
+                        });
+                    }
 
                     deferred.resolve(result);
                 }, function(error) {
