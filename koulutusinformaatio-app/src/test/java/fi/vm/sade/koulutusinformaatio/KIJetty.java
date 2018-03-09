b3b919830bf87e6fbd171e2439952e47bae2ee8f
August Kilponen <august.kilponen@gmail.com>
2018-03-09 18:14:58 +0200

    Prerenderin filter käyttöön ja KI:n oma SEO-filter pois

--- koulutusinformaatio-app/src/test/java/fi/vm/sade/koulutusinformaatio/KIJetty.java
+++ koulutusinformaatio-app/src/test/java/fi/vm/sade/koulutusinformaatio/KIJetty.java
@@ -8,12 +8,13 @@ public class KIJetty {
     static final String KI_MODULE_ROOT = ProjectRootFinder.findProjectRoot() + "/koulutusinformaatio-app";
 
     public static void main(String[] args) throws Exception {
+        System.setProperty("PRERENDER_SERVICE_URL", "http://localhost:3000/");
+
         Server server = new Server(8080);
 
         WebAppContext context = new WebAppContext();
         context.setDescriptor("src/main/webapp/WEB-INF/web.xml");
         context.setResourceBase(KI_MODULE_ROOT + "/src/main/webapp/");
-//        context.setContextPath("/koulutusinformaatio-app");
         context.setContextPath("/");
         context.setParentLoaderPriority(true);
         server.setHandler(context);
