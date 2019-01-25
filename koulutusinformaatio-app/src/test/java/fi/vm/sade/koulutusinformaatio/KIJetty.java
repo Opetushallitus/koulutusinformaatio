6e21feff511e12c25540991707014c0503c0b92d
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-01-25 10:41:58 +0200

    BUG-1943 : Limit local Jetty threads
    By default there seems to be a thread pool in use
    that expands a lot, so that thread exhaustion is
    more difficult to reproduce.

--- koulutusinformaatio-app/src/test/java/fi/vm/sade/koulutusinformaatio/KIJetty.java
+++ koulutusinformaatio-app/src/test/java/fi/vm/sade/koulutusinformaatio/KIJetty.java
@@ -1,7 +1,12 @@
 package fi.vm.sade.koulutusinformaatio;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.thread.ExecutorThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class KIJetty {
 
@@ -11,6 +16,9 @@ public class KIJetty {
         System.setProperty("PRERENDER_SERVICE_URL", "http://localhost:3000/");
 
         Server server = new Server(8080);
+        ExecutorService executorService =
+            Executors.newFixedThreadPool(10, new CustomizableThreadFactory("ki-test-jetty-"));
+        server.setThreadPool(new ExecutorThreadPool(executorService));
 
         WebAppContext context = new WebAppContext();
         context.setDescriptor("src/main/webapp/WEB-INF/web.xml");
