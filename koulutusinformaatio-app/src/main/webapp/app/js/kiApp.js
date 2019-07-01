61eba66ce7935523bc715f5c15fffdeb5a182582
teo.mertanen <teo.mertanen@gofore.com>
2019-07-01 15:24:51 +0300

    OK-241 add organization oid prefix to caller-id in frontend

--- koulutusinformaatio-app/src/main/webapp/app/js/kiApp.js
+++ koulutusinformaatio-app/src/main/webapp/app/js/kiApp.js
@@ -21,7 +21,7 @@ var kiApp = angular.module('kiApp',
         'pascalprecht.translate'
     ])
 .run(function($http, $cookies) {
-    $http.defaults.headers.common['Caller-Id'] = "koulutusinformaatio.koulutusinformaatio-app.frontend";
+    $http.defaults.headers.common['Caller-Id'] = "1.2.246.562.10.00000000001.koulutusinformaatio.koulutusinformaatio-app.frontend";
     if($cookies['CSRF']) {
         $http.defaults.headers.common['CSRF'] = $cookies['CSRF'];
     }
